542a915f3d8a11f4a6a811e0aeb95825156ff8c9
Areg <aregghaz@gmail.com>
2024-01-24 21:23:48 +0400

    design

--- src/api/admin-api/admin-api.ts
+++ src/api/admin-api/admin-api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const fakeUrl = 'http://127.0.0.1:8000'
+///const fakeUrl = 'http://127.0.0.1:8000'
+const fakeUrl = 'http://brand.speedshop.am'
 export const AdminApi = {
     dashboard() {
         return axios.get(`${fakeUrl}/api/admin/dashboard`).then(res => res.data);
